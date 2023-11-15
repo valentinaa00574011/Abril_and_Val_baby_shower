4cdd372d8eb12241f2484335034af77d43b620be
valentinaa00574011 <145053296+valentinaa00574011@users.noreply.github.com>
2023-11-14 19:29:01 -0600

    Create Password_registration.py

---
+++ Password_registration.py
@@ -0,0 +1,12 @@
+print("Password registration, you can register your 8 character password here. Remember to only use numbers and letters")
+
+one = input("Enter the first character of your password ")
+two = input("Enter the second character of your password ")
+three = input("Enter the next character of your password ")
+four = input("Enter the next character of your password ")
+five = input("Enter the next character of your password ")
+six = input("Enter the next character of your password ")
+seven = input("Enter the next character of your password ")
+eight = input("Enter the next character of your password ")
+
+print("Great, your new password is", one,two,three,four,five,six,seven,eight)
